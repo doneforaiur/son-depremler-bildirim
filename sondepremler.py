3bcac5ec1a9fc3a80f4c3c3f27511ca8e29a9e36
Mirza Atlı <mirza.a.96@hotmail.com>
2020-07-13 14:28:36 +0300

    uzaklık falan

--- sondepremler.py
+++ sondepremler.py
@@ -6,6 +6,7 @@ import locale
 import os
 import operator
 import urllib.parse
+from math import sqrt, sin, cos, sqrt, atan2, radians
 
 # Türkçe tarih formatı ve karakerler için 
 # locale'yi tr_tr.utf8 yapıyorum.
@@ -13,87 +14,121 @@ locale.setlocale(locale.LC_ALL, 'tr_TR.utf8')
 
 # Kullanılacak değişkenler falan
 temp_file        = '{}/.cache/son-depremler.json'.format(os.getenv("HOME"))
-notification_str = "{4} tarihinde\n{0} bölgesinde büyüklükleri:\nML: {1} \nMW: {2} \nMD: {3} olan bir deprem gerçekleşti."
+notification_str = "{4} tarihinde\n{0} bölgesinde büyüklükleri:\nML: {1} \nMW: {2} \nMD: {3} olan bir deprem gerçekleşti. {5} kilometre ötede."
 cmd_template     = "notify-send --urgency {} '{}'"
 notification     = ""
 last_quakes      = []
 magnitudes       = []
-api_key          = os.getenv('TELEGRAM_API_KEY')
-endpoint         = "https://api.telegram.org/bot{0}".format(api_key)
-chat_id          = os.popen("curl {}/getUpdates | jq '.result[-1].message.chat.id'".format(endpoint)).read();
+distances        = []
+
+
 
 # İsteği yap
 req = requests.get('http://kandilli-son-depremler-api.herokuapp.com/')
 # Bildirim öncelik parametresini al
 def get_urgency(_magnitude):
-	if (_magnitude >= 5.0): 
-		return 'critical'
-	elif (_magnitude >= 3.0):
-		return 'normal'
-	else:
-		return 'low'
+        if (_magnitude >= 5.0): 
+                return 'critical'
+        elif (_magnitude >= 3.0):
+                return 'normal'
+        else:
+                return 'low'
 # Bunu neden ayrı yaptım bilmiyorum
 def get_biggest(_mags):
-	return max(_mags.items(), key=operator.itemgetter(1))[0]
+        return max(_mags.items(), key=operator.itemgetter(1))[0]
+
+
+
+def get_dist(_coords):
+    coord = [39.934153, 32.873296] #dont look it up
+    _coords = _coords.split(",")
+    _coords = [float(i) for i in _coords]
+
+    #Dünyanın çapı
+    R = 6373.0
+
+    lat1 = radians(coord[0])
+    lon1 = radians(coord[1])
+    lat2 = radians(_coords[0])
+    lon2 = radians(_coords[1])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    # Eratosthenes of Cyrene, deden ölsün
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c
+
+
+
 # İstek gövdesini json'a dönüştür ve ilk 10 elemanı dön
-for quake in req.json()[slice(10)]:
-	# büyüklükleri float olarak ayıkla
-	# daha sonra aralarından büyük olanı alacağız
-	mags = {
-		'ml': float(quake['ml'] or 0),
-		'mw': float(quake['mw'] or 0),
-		'md': float(quake['md'] or 0)
-	}
-	# Alacağız demiştim.
-	biggest = get_biggest(mags)
-	# Genel büyüklükler listesine en büyük M* değerini ekle
-	# Üç büyüklük türünden en büyük olanı alıyoruz.
-	magnitudes.append(quake[biggest])
-	# Bildirim metnine verdiğimiz parametreleri yerleştiriyoruz.
-	last_quakes.append(notification_str.format(
-		*(
-			str(quake['address']),
-			str(quake['ml'] or 0),
-			str(quake['mw'] or 0),
-			str(quake['md'] or 0),
-			# Lanet tarih formatlama işleri
-			str(datetime
-				.datetime
-				.strptime(quake['timestamp'], '%Y.%m.%d %H:%M:%S')
-				.strftime('%d %B %Y %A, %H:%M'))
-			# Burada bitiyor. 
-		)
-	))
+smol_dist = float('inf') #global variable oh no
+for quake in req.json()[slice(30)]:
+        # büyüklükleri float olarak ayıkla
+        # daha sonra aralarından büyük olanı alacağız
+        mags = {
+                'ml': float(quake['ml'] or 0),
+                'mw': float(quake['mw'] or 0),
+                'md': float(quake['md'] or 0)
+        }
+        dist = get_dist(quake['geolocation'])
+        if dist < smol_dist:
+            smol_dist = dist
+        # Alacağız demiştim.
+        biggest = get_biggest(mags)
+        # Genel büyüklükler listesine en büyük M* değerini ekle
+        # Üç büyüklük türünden en büyük olanı alıyoruz.
+        magnitudes.append(quake[biggest])
+        distances.append(dist)
+        # Bildirim metnine verdiğimiz parametreleri yerleştiriyoruz.
+        last_quakes.append(notification_str.format(
+                *(
+                        str(quake['address']),
+                        str(quake['ml'] or 0),
+                        str(quake['mw'] or 0),
+                        str(quake['md'] or 0),
+                        # Lanet tarih formatlama işleri
+                        str(datetime
+                                .datetime
+                                .strptime(quake['timestamp'], '%Y.%m.%d %H:%M:%S')
+                                .strftime('%d %B %Y %A, %H:%M')),
+                        # Burada bitiyor.
+                        "{:.2f}".format(dist)
+                )
+        ))
 
 # Gelen ilk 10 satırdan en şiddetli olanın büyüklük türünü (ml,mw,md neyse işte) alıyoruz.
 mag           = max(magnitudes)
+dist          = min(distances)
 # En şiddetli olanı bildirimler listesinden çekip alıyoruz.
 biggest_index = magnitudes.index(mag)
+closest_index = distances.index(dist)
+
 
 # Bu bildirim daha önce gösterilmişse bu script biter.
 if os.path.exists(temp_file):
-	with open(temp_file, 'r') as outfile:
-		if last_quakes[biggest_index] in json.load(outfile):
-			exit()
+        with open(temp_file, 'r') as outfile:
+                if last_quakes[biggest_index] in json.load(outfile):
+                    exit()
+
+
+
 
 # Bildirim komutunu çalıştır.
 message = cmd_template.format(
-		*(
-			get_urgency(float(mag or 0)), # Önceliği al. 5 ve üstü kritik, 3-5 arası normal, kalanı düşük
-			last_quakes[biggest_index]    # Formatlanmış bildirim metnini al
-	 	)
-	)
+                *(
+                        get_urgency(float(mag or 0)), # Önceliği al. 5 ve üstü kritik, 3-5 arası normal, kalanı düşük
+                        last_quakes[closest_index]    # Formatlanmış bildirim metnini al
+                )
+        )
 result  = os.system(message)
-
-telegram_result = requests.get(url = "{}/sendMessage".format(endpoint), data = {
-	'chat_id':chat_id,
-	'text': last_quakes[biggest_index]
-})
 
 # 0 döner. dönmezse hata vardır.
 if result != 0:
-	exit()
+        exit()
 else:
-	# Gösterilen bildirimi cache'e kaydetsin. spam yapmasın.
-	with open(temp_file, 'w') as outfile:
-		json.dump([last_quakes[biggest_index]], outfile)
+        # Gösterilen bildirimi cache'e kaydetsin. spam yapmasın.
+        with open(temp_file, 'w') as outfile:
+                json.dump([last_quakes[biggest_index]], outfile)
